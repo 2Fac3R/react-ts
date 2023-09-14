2b190486baf3a3d73c249a0ee7321c9cc977869f
Gustavo Navarro <genr1818@gmail.com>
2023-09-14 14:07:55 -0600

    Reference types

--- src/app/components/RandomFox.tsx
+++ src/app/components/RandomFox.tsx
@@ -1,10 +1,15 @@
+import { useRef } from "react";
+
 type Props = {
   image: string
 }
 
 export function RandomFox({ image }: Props): JSX.Element {
+  const node = useRef<HTMLImageElement>(null)
+
   return (
     <img
+      ref={node}
       width="320"
       height="auto"
       src={image}
