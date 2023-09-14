5311f9679350bb415d80b7ba999ad826d1c37e7f
Gustavo Navarro <genr1818@gmail.com>
2023-09-14 14:01:00 -0600

    DOM types and button addNewFox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useState } from "react";
+import type { MouseEventHandler } from 'react'
 import { RandomFox } from "./components/RandomFox";
 
 const random = (): number => Math.floor(Math.random() * 123) + 1
@@ -11,14 +12,22 @@ type ImageItem = {
 }
 
 export default function Home() {
-  const [images, setImages] = useState<Array<ImageItem>>([
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-  ]);
+  const [images, setImages] = useState<Array<ImageItem>>([]);
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = () => {
+    const id = generateId();
+    const url = `https://randomfox.ca/images/${random()}.jpg`;
+    setImages([...images, { id, url }]);
+  };
+
   return (
     <main>
+      <button
+        onClick={addNewFox}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+      >
+        Add new fox
+      </button>
       {images.map(image => (
         <div className="p-4" key={image.id}>
           <RandomFox image={image.url} />
