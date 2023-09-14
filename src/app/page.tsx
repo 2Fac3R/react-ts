2ee9cef00ed372572608de7a0223059b83727139
Gustavo Navarro <genr1818@gmail.com>
2023-09-14 13:37:13 -0600

    state typed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
+"use client"
+import { useState } from "react";
 import { RandomFox } from "./components/RandomFox";
 
 const random = (): number => Math.floor(Math.random() * 123) + 1
 
 export default function Home() {
-  const image = `https://randomfox.ca/images/${random()}.jpg`;
+  const [images, setImages] = useState<Array<string>>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
   return (
     <main>
-      <RandomFox image={image} />
+      {images.map((image, index) => (
+        <div className="p-4" key={index}>
+          <RandomFox image={image} />
+        </div>
+      ))}
     </main>
   )
 }
