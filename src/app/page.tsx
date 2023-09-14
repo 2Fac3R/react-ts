115fd784442e1fe120b6ee2b8230edae870570c2
Gustavo Navarro <genr1818@gmail.com>
2023-09-14 13:52:34 -0600

    type ImageItem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,25 @@ import { useState } from "react";
 import { RandomFox } from "./components/RandomFox";
 
 const random = (): number => Math.floor(Math.random() * 123) + 1
+const generateId = () => Math.random().toString(36).substr(2, 9)
+
+type ImageItem = {
+  id: string
+  url: string
+}
 
 export default function Home() {
-  const [images, setImages] = useState<Array<string>>([
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
+  const [images, setImages] = useState<Array<ImageItem>>([
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
   ]);
   return (
     <main>
-      {images.map((image, index) => (
-        <div className="p-4" key={index}>
-          <RandomFox image={image} />
+      {images.map(image => (
+        <div className="p-4" key={image.id}>
+          <RandomFox image={image.url} />
         </div>
       ))}
     </main>
